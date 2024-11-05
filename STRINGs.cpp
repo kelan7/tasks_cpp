e97d5f5457f1b01080d9004fb6953482380ff809
Kelan VV <bsarev.vv@gmail.com>
2024-11-05 23:40:27 +0200

    first commit

---
+++ STRINGs.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<string>
+
+
+
+
+int main()
+{
+	std::string str = "hello";
+	std::string::size_type length = str.size();
+	std::string s = "k";
+	//std::string::size_type i = str.find_last_of(s);
+	size_t i = str.find_last_of(s);
+	//size_t is = str.find_first_of(s);
+	std::cout << i << std::endl;
+	return 0;
+}
+
